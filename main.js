34e3bdbe6552a6e01fb4e0022eff597bf79ee950
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-11-26 00:26:39 +0100

    exercise

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-/* global $ */
+/* API https://randomuser.me/api/ */
 
-const countries = {
+/* DICTIONARY */
+var countries = {
   AU: 'Australia',
   BR: 'Brazil',
   CA: 'Canada',
@@ -18,32 +19,3 @@ const countries = {
   TR: 'Turkey',
   US: 'United States of America'
 }
-
-const showData = () => {
-  $.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: (data) => {
-      console.log(data)
-      const { registered, dob, gender, nat, email, phone, location, picture } = data.results[0]
-      
-      const parsedRegDate = registered.split(' ')[0].split('-').join('/')
-      const parsedDateBirth = dob.split(' ')[0].split('-').join('/')
-
-      $('.registration-date').text(parsedRegDate)
-      $('.born-date').text(parsedDateBirth)
-      $('.gender').text(gender)
-      $('.address').text(location.street)
-      $('.email').text(email)
-      $('.phone').text(phone)
-      $('.profile-image').attr('src', picture.large)
-      $('.nationality').html(`<i class="flag flag-icon flag-icon-${nat.toLowerCase()}"></i><span>${countries[data.results[0].nat]}</span>`)
-    }
-  })
-}
-
-showData()
-
-$('#button').on('click', (e) => {
-  showData()
-})
